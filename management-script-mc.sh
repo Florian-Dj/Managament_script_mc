0c866650f21326dcdbf55ca64e87cdc0c9d0b839
Mucral <mucral@free.fr>
2020-02-24 17:54:10 +0000

    Add commentary management-script-mc.sh

--- management-script-mc.sh
+++ management-script-mc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# read -p "Name Project: " name
 # useradd $name --home /home/$name --shell /bin/bash
 
+# Choose Project Name
 read -p 'Project Name: ' name
 
+#Choose version minecraft server
 choose_version () {
 echo """
 Choose Version :
@@ -27,18 +28,26 @@ esac
 
 choose_version
 
+# Download java-server.jar
 wget -O java-server.jar https://papermc.io/api/v1/paper/$version/latest/download
 echo "Minecraft Server PaperMc $version Download !"
 
+# Create folder minecraft and folder project name
 mkdir /opt/minecraft/
 mkdir /opt/minecraft/$name/
 
+# Change owner and move java-server.jar
 chown minecraft: java-server.jar
 mv java-server.jar /opt/minecraft/$name/java-server.jar
 
+# Copy/Paste and change owner mc-run.sh
 cp mc-run.sh /opt/minecraft/$name/mc-run.sh
 chown minecraft: /opt/minecraft/$name/mc-run.sh
 
+# Copy/Paste eula.txt
 cp eula.txt /opt/minecraft/$name/eula.txt
 
-cp minecraft-template.service /usr/lib/systemd/system/minecraft-"$name".service
+# Copy/Pasten change variable and move minecraft.service
+cp minecraft-template.service minecraft-"$name".service
+sed -i "s/srv-name/$name/g" minecraft-"$name".service
+mv minecraft-"$name".service /usr/lib/systemd/system/minecraft-"$name".service
