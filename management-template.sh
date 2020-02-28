dec071874700141d4b3e1869b3b87766c0a42554
Mucral <mucral@free.fr>
2020-02-28 20:53:29 +0000

    Beginning management-template.sh choose action for server

--- management-template.sh
+++ management-template.sh
@@ -1,5 +1,6 @@
 # Management Server name
 
+mng_srv(){
 echo "Management Serveur name:
 
 1 - Start
@@ -8,10 +9,35 @@ echo "Management Serveur name:
 0 - Exit
 "
 
+read -p "Choose action : " choose
+case $choose in
+    1)  cd /opt/minecraft/instances/name
+	/usr/bin/screen -dmS mc-srv-name /bin/bash mc-run.sh
+        ;;
+    2)  cd /opt/minecraft/instances/name
+	/usr/bin/screen -p 0 -S mc-srv-name -X eval 'stuff "say SERVER SHUTTING DOWN. Saving map..."\\015'
+	/usr/bin/sleep 5
+	/usr/bin/screen -p 0 -S mc-srv-srv-name -X eval 'stuff "save-all"\\015'
+	/usr/bin/screen -p 0 -S mc-srv-srv-name -X eval 'stuff "stop"\\015'
+	/bin/sleep 2
+	;;
+    3)  cd /opt/minecraft/instances/name
+	/usr/bin/screen -p 0 -S mc-srv-name -X eval 'stuff "say SERVER SHUTTING DOWN. Saving map..."\\015'
+	/usr/bin/sleep 5
+	/usr/bin/screen -p 0 -S mc-srv-srv-name -X eval 'stuff "save-all"\\015'
+	/usr/bin/screen -p 0 -S mc-srv-srv-name -X eval 'stuff "stop"\\015'
+	/bin/sleep 20
+        /usr/bin/screen -dmS mc-srv-name /bin/bash mc-run.sh
+        ;;
+    0)  exit
+	;;
+    *)  mng_srv;;
+esac
+}
+
+mng_srv
 
 <<COMMENT
-cd /opt/minecraft/instances/name
-/usr/bin/screen -dmS mc-srv-name /bin/bash mc-run.sh
 
 ExecStop=/usr/bin/screen -p 0 -S mcserv-srv-test -X eval 'stuff "say SERVER SHUTTING DOWN. Saving map..."\\015'
 ExecStop=/usr/bin/sleep 5
