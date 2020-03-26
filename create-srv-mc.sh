a877fc9d67c12eddb1839c17485d0b17e1e20b83
Mucral <mucral@free.fr>
2020-03-26 22:02:00 +0000

    File create-srv-mc.sh restructuring

--- create-srv-mc.sh
+++ create-srv-mc.sh
@@ -43,83 +43,6 @@ fi
 }
 
 
-# Check if download page is good
-download_request(){
-if [ $bukkit != "papermc" ] && [ $bukkit != "other" ]
-then
-    url_download="https://cdn.getbukkit.org/$bukkit/$bukkit-$version.jar"
-elif [ $bukkit == "papermc" ]
-then
-    url_download="https://papermc.io/api/v1/paper/$version/latest/download"
-fi
-request_url="$(curl -i -o - --silent -X GET $url_download)"
-http_status=$(echo "$request_url" | grep HTTP |  awk '{print $2}')
-if [ $http_status == "200" ]
-then
-    server_script
-fi
-}
-
-# Echo under_version for bukkit support
-choose_version(){
-number_choose=1
-for versions in ${bukkit_under_version[*]}
-do
-    echo "$number_choose - $versions"
-    ((number_choose++))
-done
-echo "0 - Back"
-
-read -p "Choose version $bukkit support: " choose
-if [ $choose -ge 1 ] && [ $choose -le ${#bukkit_under_version[*]} ]
-then
-    version=${bukkit_under_version[$choose-1]}
-    download_request
-elif [ $choose -eq 0 ]
-then
-    bukkit_support
-else
-    choose_version
-fi
-}
-
-
-# List all version and all under_version
-list_version(){
-bukkit_version=()	#set list all version
-bukkit_under_version=()	#set list all under_version
-for version_list in $table_version_bukkit
-do
-    under_version=$(echo "$version_list" | grep -oP '1\.\d+')
-    if ! [[ ${bukkit_version[*]} =~ (^|[[:space:]])"$under_version"($|[[:space:]]) ]]
-    then
-	bukkit_under_version=("${bukkit_under_version[@]}" $version_list)
-	bukkit_version=("${bukkit_version[@]}" $under_version)
-    fi
-done
-choose_version
-}
-
-
-# Check pull request http
-request_https() {
-request_url="$(curl -i -o - --silent -X GET $url_request)"
-http_status=$(echo "$request_url" | grep HTTP |  awk '{print $2}')
-if [ $http_status == "200" ]
-then
-    if [ $bukkit != "papermc" ]
-    then
-	table_version_bukkit="$(echo "$request_url" | grep -oP '<h2>1\.\d+\.\d+' | cut -c5-11)"
-    else
-	table_version_bukkit="$(echo "$request_url" | grep -oP '1\.\d+\.\d+')"
-    fi
-    list_version
-else
-    echo "Error $http_status, server web not found"
-fi
-}
-
-
 # Choose bukkit support
 bukkit_support() {
 echo '
@@ -150,6 +73,84 @@ case $number_bukkit in
 	request_https;;
     *)	bukkit_support;;
 esac
+}
+
+
+# Check pull request http
+request_https() {
+request_url="$(curl -i -o - --silent -X GET $url_request)"
+http_status=$(echo "$request_url" | grep HTTP |  awk '{print $2}')
+if [ $http_status == "200" ]
+then
+    if [ $bukkit != "papermc" ]
+    then
+	table_version_bukkit="$(echo "$request_url" | grep -oP '<h2>1\.\d+\.\d+' | cut -c5-11)"
+    else
+	table_version_bukkit="$(echo "$request_url" | grep -oP '1\.\d+\.\d+')"
+    fi
+    list_version
+else
+    echo "Error $http_status, server web not found"
+fi
+}
+
+
+# List all version and all under_version
+list_version(){
+bukkit_version=()	#set list all version
+bukkit_under_version=()	#set list all under_version
+for version_list in $table_version_bukkit
+do
+    under_version=$(echo "$version_list" | grep -oP '1\.\d+')
+    if ! [[ ${bukkit_version[*]} =~ (^|[[:space:]])"$under_version"($|[[:space:]]) ]]
+    then
+	bukkit_under_version=("${bukkit_under_version[@]}" $version_list)
+	bukkit_version=("${bukkit_version[@]}" $under_version)
+    fi
+done
+choose_version
+}
+
+
+# Echo under_version for bukkit support
+choose_version(){
+number_choose=1
+for versions in ${bukkit_under_version[*]}
+do
+    echo "$number_choose - $versions"
+    ((number_choose++))
+done
+echo "0 - Back"
+
+read -p "Choose version $bukkit support: " choose
+if [ $choose -ge 1 ] && [ $choose -le ${#bukkit_under_version[*]} ]
+then
+    version=${bukkit_under_version[$choose-1]}
+    download_request
+elif [ $choose -eq 0 ]
+then
+    bukkit_support
+else
+    choose_version
+fi
+}
+
+
+# Check if download page is good
+download_request(){
+if [ $bukkit != "papermc" ] && [ $bukkit != "other" ]
+then
+    url_download="https://cdn.getbukkit.org/$bukkit/$bukkit-$version.jar"
+elif [ $bukkit == "papermc" ]
+then
+    url_download="https://papermc.io/api/v1/paper/$version/latest/download"
+fi
+request_url="$(curl -i -o - --silent -X GET $url_download)"
+http_status=$(echo "$request_url" | grep HTTP |  awk '{print $2}')
+if [ $http_status == "200" ]
+then
+    server_script
+fi
 }
 
 
