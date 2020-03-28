612208b29b2061e5bfa565788b15f3e72bfcc201
Mucral <mucral@free.fr>
2020-03-28 10:34:39 +0000

    Delete server management on management-remplate

--- file-srv/management-template.sh
+++ file-srv/management-template.sh
@@ -1,11 +1,23 @@
 # Management Server name
 
+
+check(){
+if [ "$(whoami)" == "minecraft" ]
+then
+    mng_srv
+else
+    echo "Permision denied ! Connect to minecraft user !"
+fi
+}
+
+
 mng_srv(){
 echo "Management Serveur name:
 
 1 - Start
 2 - Stop
 3 - Restart
+4 - Settings
 0 - Exit
 "
 
@@ -29,14 +41,44 @@ case $choose in
 	/bin/sleep 20
         /usr/bin/screen -dmS mc-srv-name /bin/bash mc-run.sh
         ;;
+    4)	settings
+	;;
     0)  exit
 	;;
     *)  mng_srv;;
 esac
 }
-if [ "$(whoami)" == "minecraft" ]
-then
-    mng_srv
-else
-    echo "Permision denied ! Connect to minecraft user !"
-fi
+
+
+settings(){
+echo "Setting for Serveur name:
+
+1 - Delete
+2 - Ram
+0 - Back
+"
+
+read -p "Chosse action : " choose
+case $choose in
+    1)	delete
+	;;
+    2)	echo "Ram"
+	;;
+    0)	mng_srv
+	;;
+esac
+}
+
+delete(){
+read -p "Warning! This action will delete server. Are you sure ? (yes/no) " choose
+case $choose in
+    yes)rm -rf /opt/minecraft/instances/name
+	rm ~/instances/management-name.sh;;
+    no) settings
+	;;
+    *)	delete
+	;;
+esac
+}
+
+check
